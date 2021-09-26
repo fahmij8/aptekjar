94a9794aa5d41bde72b5c14a40760d691094c3e9
fahmij8 <fahmijabbar@upi.edu>
2021-09-26 19:27:39 +0700

    Add Livewire Components

---
+++ app/Http/Livewire/Offline.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Offline extends Component
+{
+    public function render()
+    {
+        return view('livewire.offline');
+    }
+}
---
+++ resources/views/livewire/offline.blade.php
@@ -0,0 +1,10 @@
+<div wire:offline>
+    <div id="toastsContainerTopRight" class="toasts-top-right fixed mt-2 mr-2">
+        <div class="toast bg-danger fade show" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <strong class="mr-auto">Warning : You're offline</strong>
+            </div>
+            <div class="toast-body">Please turn your connectivity back on to continue.</div>
+        </div>
+    </div>
+</div>
